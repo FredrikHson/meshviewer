3c96ace3d25fdb3d436a04284d86d82d3dbd9c2c
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2021-03-17 23:22:12 +0100

    config file support

--- meshviewer.js
+++ meshviewer.js
@@ -1,4 +1,5 @@
 print("initing");
+RAD = 0.0174532925199;
 filename = getoptionalstring("file", "");
 
 shadowbuffer = createrendertarget(1024, 1024, 1, GL_RGBA, GL_RGBA32F, 0);
@@ -12,6 +13,9 @@ else
     mesh = loadmesh(filename);
 }
 
+//re = new RegExp("/(?:\.([^.]+))?$/","i");
+ext = filename.substr(filename.lastIndexOf('.') + 1);
+
 bbox = getmeshbbox(mesh);
 meshshader = loadshader("mesh.vert", "mesh.frag", "mesh.geom", 0, 0);
 shadowbufshader = loadshader("shadowbuf.vert", "shadowbuf.frag", 0, 0, 0);
@@ -20,11 +24,68 @@ blit = loadshader("blit.vert", "blit.frag", 0, 0, 0);
 
 plane = generateplane(50);
 
-//cube = loadmesh("cube.stl");
-rotx = 0.785398;
-roty = -0.785398 / 2;
-lightx = 0;
-lighty = 0;
+clearcolor = [ 0, 0, 0, 0 ];
+matcolor = [1, 1, 1];
+angle = [45, 22.5];
+lightdir = [0, 0];
+zoom = 0;
+
+function readconfigvalue(configname, defvalue)
+{
+    if(config == undefined)
+    {
+        return defvalue;
+    }
+
+    if(config.defaults != undefined)
+    {
+        if(config.defaults.matcolor != undefined)
+        {
+            finalvalue = config.defaults[configname];
+        }
+    }
+
+    if(config[ext][configname] != undefined)
+    {
+        finalvalue = config[ext][configname];
+    }
+
+    if(finalvalue != undefined)
+    {
+        return finalvalue;
+    }
+    else
+    {
+        return defvalue;
+    }
+}
+
+function loadconfig()
+{
+    try
+    {
+        jsonstring = File.read("confige.json");
+        config = JSON.parse(jsonstring);
+        jsonstring = 0;
+        matcolor = readconfigvalue("matcolor", [1, 1, 1]);
+        angle = readconfigvalue("angle", [45, -22.5]);
+        angle[0] *= RAD;
+        angle[1] *= RAD;
+        lightdir = readconfigvalue("lightangle", [0, 0]);
+        zoom = readconfigvalue("zoom", -2);
+        clearcolor = readconfigvalue("clear", [0, 0, 0, 1]);
+    }
+    catch(error)
+    {
+    }
+
+    print(config);
+}
+
+loadconfig();
+
+posx = 0;
+posy = 0;
 
 center =
 {
@@ -54,17 +115,29 @@ print(bbox);
 print(center);
 up = 3;
 step = 0;
-zoom = -1.5;
-function loop()
+
+function handleinput()
 {
+    if(KEY_R & PRESSED_NOW)
+    {
+        loadconfig();
+    }
+
+    if(KEY_F & PRESSED_NOW)
+    {
+        posx = 0;
+        posy = 0;
+        zoom = -1.5;
+    }
+
     if((KEY_LEFT_SHIFT & PRESSED || KEY_RIGHT_SHIFT & PRESSED))
     {
         if(MOUSE_INSIDE)
         {
             if(MOUSE_1 & PRESSED)
             {
-                lightx -= MOUSE_DELTA_X * 0.01;
-                lighty -= MOUSE_DELTA_Y * 0.01;
+                lightdir[0] -= MOUSE_DELTA_X * 0.01;
+                lightdir[1] -= MOUSE_DELTA_Y * 0.01;
             }
         }
 
@@ -89,8 +162,8 @@ function loop()
         {
             if(MOUSE_1 & PRESSED)
             {
-                rotx += MOUSE_DELTA_X * 0.01;
-                roty += MOUSE_DELTA_Y * 0.01;
+                angle[0] += MOUSE_DELTA_X * 0.01;
+                angle[1] += MOUSE_DELTA_Y * 0.01;
             }
         }
 
@@ -109,12 +182,22 @@ function loop()
             up = 1;
         }
 
+        if(MOUSE_3 & PRESSED)
+        {
+            posx -= MOUSE_DELTA_X * 0.004;
+            posy += MOUSE_DELTA_Y * 0.004;
+        }
+
         if(MOUSE_2 & PRESSED)
         {
             zoom += MOUSE_DELTA_Y * 0.01;
         }
     }
+}
 
+function loop()
+{
+    handleinput();
     lightvector = {x: 0, y: 0, z: 1};
     model = mat4loadidentity();
     model = mat4mul(model, mat4settranslation(-center.x, -center.y, -center.z));
@@ -143,14 +226,14 @@ function loop()
             break;
     }
 
-    model = mat4mul(model, mat4setrotation(rotx, 0, 1, 0));
-    model = mat4mul(model, mat4setrotation(roty, 1, 0, 0));
+    model = mat4mul(model, mat4setrotation(angle[0], 0, 1, 0));
+    model = mat4mul(model, mat4setrotation(angle[1], 1, 0, 0));
     shadowmat = model;
-    shadowmat = mat4mul(shadowmat, mat4setrotation(lightx, 0, 1, 0));
-    shadowmat = mat4mul(shadowmat, mat4setrotation(lighty, 1, 0, 0));
+    shadowmat = mat4mul(shadowmat, mat4setrotation(lightdir[0], 0, 1, 0));
+    shadowmat = mat4mul(shadowmat, mat4setrotation(lightdir[1], 1, 0, 0));
     lightmat = mat4loadidentity();
-    lightmat  = mat4mul(lightmat, mat4setrotation(lightx, 0, 1, 0));
-    lightmat  = mat4mul(lightmat, mat4setrotation(lighty, 1, 0, 0));
+    lightmat  = mat4mul(lightmat, mat4setrotation(lightdir[0], 0, 1, 0));
+    lightmat  = mat4mul(lightmat, mat4setrotation(lightdir[1], 1, 0, 0));
     lightvector = vec3mat4mul(lightvector, mat4invert((lightmat)));
     beginpass(shadowbuffer);
     {
@@ -180,9 +263,9 @@ function loop()
     {
         depthtest(1);
         culling(CULL_NONE);
-        clear(0.0, 0.0, 0.0, 0.7);
+        clear(clearcolor[0], clearcolor[1], clearcolor[2], clearcolor[3]);
         cleardepth();
-        view = mat4settranslation(0, 0, zoom);
+        view = mat4settranslation(posx, posy, zoom);
         persp = mat4setperspective(0.785398, RENDER_WIDTH / RENDER_HEIGHT, 0.1, 1000.0);
         bindshader(meshshader);
         bindattribute("in_Position", MESH_FLAG_POSITION);
@@ -191,6 +274,7 @@ function loop()
         setuniformmat4("shadowmodelview", mat4mul(shadowmat, view));
         setuniformmat4("persp", persp);
         setuniformf("lightvector", lightvector.x, lightvector.y, lightvector.z);
+        setuniformf("materialcolor", matcolor[0], matcolor[1], matcolor[2]);
         drawmesh(mesh);
         bindshader(-1);
     }
